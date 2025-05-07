cdc91287b79f2a4381f9836075494c1f5dbda932
michi <michi@zulo.local>
2025-05-07 19:27:12 +0200

    elimino retrofit

--- app/src/main/java/local/a24miguelod/cookflow/model/retrofit/RecetasAPI.kt
+++
@@ -1,8 +0,0 @@
-package local.a24miguelod.cookflow.model.retrofit
-
-import retrofit2.http.GET
-
-//interface EquipoAPI {
-//    @GET("recetas")
-//    suspend fun getRecetas(): Recetas
-//}
--- app/src/main/java/local/a24miguelod/cookflow/model/retrofit/gson/Recetas.kt
+++
@@ -1,9 +0,0 @@
-package local.a24miguelod.cookflow.model.retrofit.gson
-
-import local.a24miguelod.cookflow.model.Receta
-
-class Recetas(val recetas: MutableList<Receta> = mutableListOf<Receta>()) {
-    fun getAsList() : List<Receta> {
-        return recetas
-    }
-}
